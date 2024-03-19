32627913cda5d885d7fd18d097c0cff141a12aa2
dassepascal <pascaldasse56@gmail.com>
2024-03-19 09:32:25 +0100

    commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import IncrementCounter from './components/IncrementCounter.vue';
+
+
+
+createApp({})
+    .component('IncrementCounter', IncrementCounter)
+
+
+    .mount('#app')
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container bg-red-400">
+        <div class="row">
+            <div class="col-md-12 bg-red-400">
+                <h1 class="text-2xl">Welcome to the Laravel 5.4 Blog</h1>
+
+            </div>
+        </div>
+
+    </div>
+@endsection
+
